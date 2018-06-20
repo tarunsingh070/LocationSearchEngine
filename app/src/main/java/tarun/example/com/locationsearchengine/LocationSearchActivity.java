a02313d0a9bb2f27652f3eee3208fd578c9449d2
tarunsingh070 <31713053+tarunsingh070@users.noreply.github.com>
2018-06-20 00:19:15 -0400

    Added location search functionality.
    1) Added Google Places SDK for search functionality.
    2) Added PlaceAutoCompleteFragment to display location search bar.
    3) Added dependency for Places sdk in gradle.

--- app/src/main/java/tarun/example/com/locationsearchengine/LocationSearchActivity.java
+++ app/src/main/java/tarun/example/com/locationsearchengine/LocationSearchActivity.java
@@ -2,15 +2,26 @@ package tarun.example.com.locationsearchengine;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
- * This activity displays a view with a world map.
+ * This activity displays a view with a world map and a search input field for the user to search for any location.
  */
 public class LocationSearchActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final String TAG = LocationSearchActivity.class.getSimpleName();
+    private static final float LOCATION_ZOOM_LEVEL = 10f;
 
     private GoogleMap mMap;
 
@@ -20,6 +31,7 @@ public class LocationSearchActivity extends FragmentActivity implements OnMapRea
         setContentView(R.layout.activity_location_search);
 
         setupMapFragment();
+        setupPlaceAutoCompleteFragment();
     }
 
     /**
@@ -33,6 +45,34 @@ public class LocationSearchActivity extends FragmentActivity implements OnMapRea
         mapFragment.getMapAsync(this);
     }
 
+    /**
+     * This method sets up the PlaceAutoCompleteFragment which displays the auto complete search bar
+     * at the top of the screen for user to search for locations.
+     */
+    private void setupPlaceAutoCompleteFragment() {
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // Removes the existing marker from the map.
+                mMap.clear();
+                // Add a marker along with title as info for the new location selected by the user on searching.
+                mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName().toString()));
+                // Animates the movement of the camera from the current position to the position of
+                // the selected location and set the zoom level to 10 by default.
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), LOCATION_ZOOM_LEVEL));
+            }
+
+            @Override
+            public void onError(Status status) {
+                // Something went wrong. Display an error message to the user.
+                Log.i(TAG, "An error occurred: " + status);
+                Toast.makeText(LocationSearchActivity.this, R.string.error_message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 
     /**
      * This callback is triggered when the map is ready to be used.
